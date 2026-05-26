707d44fdc124b3dc7184580bc09808a25cc0643d
Oleksandr Berger <56601624+hugonote@users.noreply.github.com>
2026-05-26 22:51:12 +0300

    🍺🍺🍺🍺

---
+++ Casks/minecraft-bedrock-launcher.rb
@@ -0,0 +1,29 @@
+cask "minecraft-bedrock-launcher" do
+  version "0.1.2"
+  sha256 "ab676d300eb8efd4755dc5f6c04aabaf4b707cc9b72eaf5dde49870d59e686da"
+
+  url "https://github.com/hugonote/mcpelauncher-swift/releases/download/v#{version}/Minecraft.Bedrock.Launcher-#{version}.dmg"
+  name "Minecraft Bedrock Launcher"
+  desc "Native SwiftUI launcher for Minecraft: Bedrock Edition on macOS"
+  homepage "https://github.com/hugonote/mcpelauncher-swift"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :sonoma"
+
+  app "Minecraft Bedrock Launcher.app"
+
+  uninstall quit: "local.minecraft.bedrock.swiftlauncher"
+
+  zap trash: [
+    "~/Library/Application Support/Minecraft Bedrock Launcher",
+    "~/Library/Preferences/local.minecraft.bedrock.swiftlauncher.plist",
+    "~/Library/Saved Application State/local.minecraft.bedrock.swiftlauncher.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+
+# 🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺
+
+homebrew tap for [`hugonote/mcpelauncher-swift`](https://github.com/hugonote/mcpelauncher-swift).
+
+
+```sh
+brew tap hugonote/mcpelauncher-swift
+brew install --cask minecraft-bedrock-launcher
+```
+
